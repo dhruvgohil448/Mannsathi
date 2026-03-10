746eee12e7f68c3be4c6d33b82fa7ff92666a06a
Dhruv Gohil <dhruvgohil448@gmail.com>
2026-03-10 14:06:31 +0530

    Remove hardcoded MongoDB URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://harryjoshi2812:Harryjoshi28@cluster0.9c1ds.mongodb.net/Mojo").then(()=>console.log("DB connected"));
-}
+export const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("DB connected"));
+};
